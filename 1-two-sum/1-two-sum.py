0cda6004d19e09f0b8e29cce84fe4a6b7686bd55
cmejustsmile <42774155+cmejustsmile@users.noreply.github.com>
2022-03-18 14:43:39 +0900

    Time: 88 ms (66.49%), Space: 15.2 MB (31.83%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,34 +1,12 @@
+#https://www.code-recipe.com/post/two-sum
+
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        newdict= {}
-        answers = [0,0]
-        for i in range(0, len(nums)):
-            if nums[i] in newdict:
-                newdict[nums[i]].insert(-1,i)
-            else:
-                newdict[nums[i]] = [i]
-
-        tmpdict = newdict.copy()
-
-        for key in newdict.keys():
-            if key in tmpdict and len(tmpdict[key]) == 0:
-                tmpdict.pop(key)
-                continue
-
-            idx = tmpdict[key].pop()
-
-            if target-key in tmpdict:
-                if len(tmpdict[target-key]) == 0:
-                    tmpdict.pop(target-key)
-                    continue
-                idx2 = tmpdict[target-key].pop()
-
-                if idx2 < idx:
-                    answers[0] = idx2
-                    answers[1] = idx
-                else:
-                    answers[0] = idx
-                    answers[1] = idx2
-                return answers
-
-        return answers
+   def twoSum(self, nums: List[int], target: int) -> List[int]:
+       seen = {}
+       for i, value in enumerate(nums):
+           remaining = target - nums[i]
+           
+           if remaining in seen:
+               return [i, seen[remaining]]
+            
+           seen[value] = i 
